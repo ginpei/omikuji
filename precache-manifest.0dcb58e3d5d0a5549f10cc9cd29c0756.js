e6e5e723f0a9341b23b260890248a0119ff1f543
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-26 05:32:48 +0000

    Deploy ginpei/omikuji to github.com/ginpei/omikuji.git:gh-pages

---
+++ precache-manifest.0dcb58e3d5d0a5549f10cc9cd29c0756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6987c1aa47cb2881d262",
+    "url": "/omikuji/index.html/static/js/runtime~main.6987c1aa.js"
+  },
+  {
+    "revision": "a04e1febb1867ac0ccbc",
+    "url": "/omikuji/index.html/static/js/main.a04e1feb.chunk.js"
+  },
+  {
+    "revision": "68f11c1e64163d3f36fe",
+    "url": "/omikuji/index.html/static/js/2.68f11c1e.chunk.js"
+  },
+  {
+    "revision": "a04e1febb1867ac0ccbc",
+    "url": "/omikuji/index.html/static/css/main.447a1395.chunk.css"
+  },
+  {
+    "revision": "a0e24a161d57c3973158e2b5036b6a53",
+    "url": "/omikuji/index.html/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/omikuji/index.html/precache-manifest.0dcb58e3d5d0a5549f10cc9cd29c0756.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/omikuji/index.html/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
