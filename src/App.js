e333e1af25a3d1d288a6d4dae147493c24f1271b
ginpei <ginpei@gmail.com>
2019-02-25 19:56:59 -0800

    first omikuji

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Omikuji from "./components/Omikuji";
 
-class App extends Component {
+const omikujiPossibilities = [
+  {
+    name: 'daikichi',
+    weight: 30,
+  },
+  {
+    name: 'kichi',
+    weight: 50,
+  },
+  {
+    name: 'daikyo',
+    weight: 20,
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Omikuji
+          omikujiPossibilities={omikujiPossibilities}
+        />
       </div>
     );
   }
---
+++ src/components/misc.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+max-width: calc(800px - 2rem);
+padding-left: 1rem;
+padding-right: 1rem;
+`;
+
+export const CenterBox = styled.div`
+align-items: center;
+display: flex;
+flex-direction: column;
+justify-content: center;
+`;
